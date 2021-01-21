ba44e322b24f45f06cbe6856f648bdc908ca105b
LarMont <pk.ivanov96@gmail.com>
2021-01-22 02:10:03 +0400

    Разнес создание ретрофита по кодеину

--- core/src/main/java/com/example/core/Kodein.kt
+++ core/src/main/java/com/example/core/Kodein.kt
@@ -10,6 +10,7 @@ import com.example.core.serverapi.TaskManager
 import com.example.core.serverapi.TaskManagerImpl
 import com.github.salomonbrys.kodein.*
 import com.github.salomonbrys.kodein.conf.ConfigurableKodein
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import okhttp3.OkHttpClient
@@ -21,7 +22,35 @@ class KodeinWorker {
     companion object {
         val kodein = ConfigurableKodein(mutable = true).apply {
             addConfig {
-                bind<ServerApi>() with singleton { createRetrofit().create(ServerApi::class.java) }
+                bind<Gson>() with singleton {
+                    GsonBuilder()
+                        .setLenient()
+                        .create()
+                }
+                bind<OkHttpClient>() with singleton {
+                    OkHttpClient.Builder()
+                        .connectTimeout(60, TimeUnit.SECONDS)
+                        .readTimeout(60, TimeUnit.SECONDS)
+                        .writeTimeout(60, TimeUnit.SECONDS)
+                        .addInterceptor { chain ->
+                            val request = chain.request()
+                            val builder = request.newBuilder()
+                            val newRequest = builder.build()
+                            Log.e("retrofit_url", newRequest.url().toString())
+                            chain.proceed(newRequest)
+                        }
+                        .build()
+                }
+                bind<ServerApi>() with singleton {
+                    Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create(getGsonConverter()))
+                        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                        .client(getHttpClient())
+                        .build()
+                        .create(ServerApi::class.java)
+                }
+
                 bind<TaskManager>() with singleton { TaskManagerImpl() }
                 bind<DBWorker>() with multiton { context: Context -> DBWorkerImpl(context) }
             }
@@ -30,30 +59,9 @@ class KodeinWorker {
 }
 
 private inline fun <reified T : Any> kodeinResolve() = KodeinWorker.kodein.instance<T>()
+fun getGsonConverter(): Gson = kodeinResolve()
+fun getHttpClient(): OkHttpClient = kodeinResolve()
 
 fun getServerApi(): ServerApi = kodeinResolve()
 fun getTaskManager(): TaskManager = kodeinResolve()
 fun getDBWorker(context: Context): DBWorker = KodeinWorker.kodein.with(context).instance()
-
-private fun createRetrofit() = Retrofit.Builder()   ////разнести по нужным местам
-    .baseUrl(BASE_URL)
-    .addConverterFactory(GsonConverterFactory.create(gson))
-    .addCallAdapterFactory(CoroutineCallAdapterFactory())
-    .client(createHttpClient())
-    .build()
-
-var gson = GsonBuilder()
-    .setLenient()
-    .create()
-
-private fun createHttpClient() = OkHttpClient.Builder()
-    .connectTimeout(60, TimeUnit.SECONDS)
-    .readTimeout(60, TimeUnit.SECONDS)
-    .writeTimeout(60, TimeUnit.SECONDS)
-    .addInterceptor { chain ->
-        val request = chain.request()
-        val builder = request.newBuilder()
-        val newRequest = builder.build()
-        Log.e("retrofit_url", newRequest.url().toString())
-        chain.proceed(newRequest)}
-    .build()
